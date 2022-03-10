304bd5f1424a2a28e4ab3504984f1e6426474ca4
comeschffr <come.schffr@gmail.com>
2022-03-10 20:40:19 +0100

    experimental way to initialize db

--- starthackapp/__init__.py
+++ starthackapp/__init__.py
@@ -9,6 +9,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace('postgres', 'postgresql')
 
 db = SQLAlchemy(app)
+import starthackapp.models
 db.create_all()
 
 import starthackapp.endpoints
