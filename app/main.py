f01c76e0143f69a3d411439cb7296a6efaa33cc0
comeschffr <come.schffr@gmail.com>
2022-03-10 17:01:31 +0100

    initial app commit

---
+++ app/main.py
@@ -0,0 +1,64 @@
+from flask import Flask, jsonify
+app= Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return jsonify(["<h1>Welcome to my app bro through github</h1>"])
+
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    return jsonify(["user alredy existssssssssssssssss"])
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    return jsonify(["login bro"])
+
+# from flask import Blueprint, request, json, jsonify
+# from .models import Student
+# from software import db
+
+# views = Blueprint('views', __name__)
+
+# @views.route('/register', methods=["GET", "POST"])
+# def register():
+#     d={}
+#     if request.method =="POST":
+#         mail = request.form["email"]
+#         password = request.form["password"]
+
+#         email = Student.query.filter_by(email=mail).first()
+
+#         if email is None:
+#             register = Student(email=mail, password=password)
+
+#             db.session.add(register)
+#             db.session.commit()
+           
+#             return jsonify(["Register success"])
+#         else:
+#             # already exist
+            
+#             return jsonify(["user alredy exist"])
+
+
+# @views.route('/login', methods=["GET", "POST"])
+# def login():
+#     d = {}
+#     if request.method == "POST":
+#         mail = request.form["email"]
+#         password = request.form["password"]
+
+#         login = Student.query.filter_by(email=mail, password=password).first()
+
+#         if login is None:
+#             # acount not found
+            
+#             return jsonify(["Wrong Credentials"]) 
+#         else:
+#             # acount found
+            
+#             return jsonify([ "success"])
