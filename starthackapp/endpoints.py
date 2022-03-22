aaeacf29a502c09c966ed5536b530017913aee26
comeschffr <come.schffr@gmail.com>
2022-03-22 23:02:48 +0100

    added Hero model and basic methods

--- starthackapp/endpoints.py
+++ starthackapp/endpoints.py
@@ -21,26 +21,51 @@ def login():
     return jsonify('Just landed on the /login endpoint')
 
 
-@app.route('/add_user', methods=["POST"])
-def add_user():
-    new_user = models.User(request.form.get('name'), request.form.get('email'))
-    db.session.add(new_user)
+# @app.route('/add_user', methods=["POST"])
+# def add_user():
+#     new_user = models.User(request.form.get('name'), request.form.get('email'))
+#     db.session.add(new_user)
+#     db.session.commit()
+#     return jsonify('Added a new user successfully!')
+
+
+# @app.route('/get_users', methods=["GET"])
+# def get_users():
+#     users = models.User.query.all()
+#     users = [
+#         {
+#             'id': user.id,
+#             'name': user.name,
+#             'email': user.email,
+#         } for user in users
+#     ]
+#     return jsonify(users)
+
+
+@app.route('/add_hero', methods=["POST"])
+def add_hero():
+    new_hero = models.Hero(request.form.get('name'), request.form.get('world'))
+    db.session.add(new_hero)
     db.session.commit()
-    return jsonify('Added a new user successfully!')
+    return jsonify('Added a new hero successfully!')
 
 
-@app.route('/get_users', methods=["GET"])
-def get_users():
-    users = models.User.query.all()
-    users = [
-        {
-            'id': user.id,
-            'name': user.name,
-            'email': user.email,
-        } for user in users
-    ]
-    return jsonify(users)
 
+@app.route('/swipe', methods=["POST"])
+def swipe():
+    form_data = request.form
+    hero_id = form_data.get('hero_id')
+    swipe_dir = form_data.get('swipe_dir')
 
+    hero = models.User.query.get(hero_id)
+    if swipe_dir == 'right':
+        hero.likeness = models.Likeness.LIKE
+    elif swipe_dir == 'left':
+        hero.likeness = models.Likeness.NO_LIKE
+    elif swipe_dir == 'up':
+        hero.likeness = models.Likeness.SUPER_LIKE
+
+    db.session.commit()
+    return jsonify('Swiped successfully!')
 
 
--- starthackapp/models.py
+++ starthackapp/models.py
@@ -1,16 +1,24 @@
 from starthackapp import app, db
+import enum
 
 
-class User(db.Model):
+class Likeness(enum.Enum):
+	LIKE = 'LIKE'
+	NO_LIKE = 'NO_LIKE'
+	SUPER_LIKE = 'SUPER_LIKE'
+
+
+class Hero(db.Model):
 	# Defines the Table Name user
-	__tablename__ = "user"
+	__tablename__ = "hero"
 
 	# Makes three columns into the table id, name, email
 	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 	name = db.Column(db.String(100), nullable=False)
-	email = db.Column(db.String(100), nullable=False)
+	world = db.Column(db.String(100))
+	likeness = db.Column(db.Enum(Likeness))
 
 	# A constructor function where we will pass the name and email of a user and it gets add as a new entry in the table.
-	def __init__(self, name, email):
+	def __init__(self, name, world=None):
 		self.name = name
-		self.email = email
+		self.world = world
