aa117a3791058e8a9a47e8f1ce785d1a693cbbc4
comeschffr <come.schffr@gmail.com>
2022-03-23 00:18:45 +0100

    new migration system

--- starthackapp/__init__.py
+++ starthackapp/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
@@ -9,6 +10,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace('postgres', 'postgresql')
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 import starthackapp.models
 db.create_all()
 
